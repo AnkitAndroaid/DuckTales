df5fe1b234a294d16fe85bba0acfd3155cbe0681
ankit-gupta6791 <91ankitgupta@gmail.com>
2023-12-06 22:09:38 +0530

    Added Basic theme components

---
+++ theme/src/main/java/com/ankit/ducktales/theme/components/DuckTalesPreview.kt
@@ -0,0 +1,10 @@
+package com.ankit.ducktales.theme.components
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(name = "Preview Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "Preview Light", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+annotation class DuckTalesPreview {
+}
